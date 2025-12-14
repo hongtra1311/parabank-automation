a0f3c651a0e56977d8473f28988fb3285529508b
Hồng Trà <hongtra@Mac.lan>
2025-12-14 21:55:22 +0700

    Add config

--- conftest.py
+++ conftest.py
@@ -1,13 +1,48 @@
 import pytest
 from selenium import webdriver
+import time, random
 from webdriver_manager.chrome import ChromeDriverManager
 from page_object_models.login_page import LoginPage
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
+def human_sleep(a=2, b=5):
+    time.sleep(random.uniform(a, b))
 
 @pytest.fixture
 def driver():
+    options = Options()
+
+    # ❌ KHÔNG headless khi test site thật
+    # options.add_argument("--headless")
+
+    options.add_argument(
+        "--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/120.0.0.0 Safari/537.36"
+    )
+
+    options.add_experimental_option(
+        "excludeSwitches", ["enable-automation"]
+    )
+    options.add_experimental_option(
+        "useAutomationExtension", False
+    )
+
+    options.add_argument("--user-data-dir=./chrome-profile")
+    options.add_argument("--profile-directory=Default")
     service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
+    driver = webdriver.Chrome(service=service, options=options)
+    driver.execute_cdp_cmd(
+        "Page.addScriptToEvaluateOnNewDocument",
+        {
+            "source": """
+            Object.defineProperty(navigator, 'webdriver', {
+                get: () => undefined
+            });
+            """
+        }
+    )
     driver.maximize_window()
     yield driver
     driver.quit()
